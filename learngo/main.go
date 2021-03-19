60cd46e11536883efbe96e3ee13d834451397e21
jeslsy0507 <jeslsy0507@naver.com>
2021-03-19 19:40:13 +0900

    complete Add()

--- learngo/main.go
+++ learngo/main.go
@@ -7,12 +7,23 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
-	if err != nil { //어떤 에러메시지 있으면
-		fmt.Println(err) // 에러메시지 출력
-	} else { // 에러메시지 없으면
-		fmt.Println(definition) //받아온 값 출력
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
+
+	// 항상 이렇게 에러처리 코드를 해줘야함
+	if err != nil {
+		fmt.Println(err)
 	}
 
+	// 위에서 추가 됐을 테니 hello 로 받아서 출력!
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found", word, "definition:", hello)
+
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	fmt.Println()
 }
--- learngo/mydict/mydict.go
+++ learngo/mydict/mydict.go
@@ -5,13 +5,26 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Now Found")
+var errNotFound = errors.New("Not Found")
+var errWordExists = errors.New("That word already exists")
 
 //Search for a word
 func (d Dictionary) Search(word string) (string, error) {
 	value, exists := d[word] //word라는 키에 해당하는 값을 반환
 	if exists {
-		return value, nil
+		return value, nil // 단어 있음 == 에러가 없다는 말임
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound: //단어 없으면
+		d[word] = def // 단어 추가
+	case nil: // 단어 있으면
+		return errWordExists //단어 존재한다고 에러메시지 반환
+	}
+	return nil // 단어 추가하고 nil 반환
 }
