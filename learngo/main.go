86ebdfd63108586701ec464caf8a195f4174fadb
jeslsy0507 <jeslsy0507@naver.com>
2021-03-20 21:48:54 +0900

    learn go routines

--- learngo/main.go
+++ learngo/main.go
@@ -1,50 +1,26 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
-
 func main() {
-	//map 타입으로 results 정의
-	var results = make(map[string]string)
+	go sexyCount("suyoung")
+	cuteCount("nico")
+	time.Sleep(time.Second * 5)
+}
 
-	urls := []string{
-		"https://www.airbnb.co.kr/",
-		"https://www.google.co.kr/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.instagram.com/",
-	}
-
-	results["gello"] = "Hello"
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil { // err있으면
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
 	}
 }
 
-// hit 못하면 에러를 반환해야 겠죠 ?
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-
-	// 상태 코드 400부터는 페이지 오류니까..!
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+func cuteCount(person string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
