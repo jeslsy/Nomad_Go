c4d21e1407d0177a665b67105f2b3c55395e0d31
jeslsy0507 <jeslsy0507@naver.com>
2021-03-20 22:12:34 +0900

    learn channels

--- learngo/main.go
+++ learngo/main.go
@@ -6,21 +6,21 @@ import (
 )
 
 func main() {
-	go sexyCount("suyoung")
-	cuteCount("nico")
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		//두번의 함수는 5초뒤 ture라는 2개의 메시지를 main에 보내줌
+		go isSexy(person, c)
+	}
 	time.Sleep(time.Second * 5)
+
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
-func sexyCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
-}
-
-func cuteCount(person string) {
-	for i := 0; i < 3; i++ {
-		fmt.Println(person, "is sexy", i)
-		time.Sleep(time.Second)
-	}
+// url 체크하고 결과를 main에 알려주는 기능(채널로) 만들어 보겠음
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	fmt.Println(person)
+	c <- true
 }
