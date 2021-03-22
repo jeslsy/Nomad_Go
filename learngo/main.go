5ef2798782302cbcaf5c577415457e4e12bd706e
jeslsy0507 <jeslsy0507@naver.com>
2021-03-22 19:37:40 +0900

    create FAST URLChecker

--- learngo/main.go
+++ learngo/main.go
@@ -1,27 +1,56 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func main() {
-	c := make(chan string)
-	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
-	for _, person := range people {
-		//두번의 함수는 5초뒤 ture라는 2개의 메시지를 main에 보내줌
-		go isSexy(person, c)
-	}
+// 지금부터 URLchecker를 채널로 보낸대
+// 진짜 최고 빠름.
 
-	for i := 0; i < len(people); i++ {
-		fmt.Print("waiting for", i)
-		fmt.Println(<-c)
-	}
-
+type requestResult struct {
+	url    string
+	status string
 }
 
-// url 체크하고 결과를 main에 알려주는 기능(채널로) 만들어 보겠음
-func isSexy(person string, c chan string) {
-	time.Sleep(time.Second * 2)
-	c <- person + "is sexy"
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	results := make(map[string]string)
+	c := make(chan requestResult)
+	urls := []string{
+		"https://www.airbnb.co.kr/",
+		"https://www.google.co.kr/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.instagram.com/",
+	}
+
+	//각 URL을 hitURL()한걸 채널에 넣어줌
+	for _, url := range urls {
+		go hitURL(url, c)
+	}
+
+	//채널에서 result라는 곳에 값(statusCode) 받아옴
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		// results 맵에 값 저장
+		results[result.url] = result.status
+	}
+
+	// URL이랑 statusCode 값 출력
+	for url, status := range results {
+		fmt.Println(url, status)
+	}
+}
+
+// chan <- result 하면 sending only라는걸 보여주는 것!
+func hitURL(url string, c chan<- requestResult) {
+	resp, err := http.Get(url)
+	status := "OK"
+	if err != nil || resp.StatusCode >= 400 {
+		status = "FAILED"
+	}
+	c <- requestResult{url: url, status: status}
 }
