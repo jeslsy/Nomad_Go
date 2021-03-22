03c75fdfd8b30763a0a14dc659da38afa0473f6f
jeslsy0507 <jeslsy0507@naver.com>
2021-03-22 19:00:37 +0900

    basic channel

--- learngo/main.go
+++ learngo/main.go
@@ -6,21 +6,22 @@ import (
 )
 
 func main() {
-	c := make(chan bool)
-	people := [2]string{"nico", "flynn"}
+	c := make(chan string)
+	people := [5]string{"nico", "flynn", "dal", "japanguy", "larry"}
 	for _, person := range people {
 		//두번의 함수는 5초뒤 ture라는 2개의 메시지를 main에 보내줌
 		go isSexy(person, c)
 	}
-	time.Sleep(time.Second * 5)
 
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	for i := 0; i < len(people); i++ {
+		fmt.Print("waiting for", i)
+		fmt.Println(<-c)
+	}
+
 }
 
 // url 체크하고 결과를 main에 알려주는 기능(채널로) 만들어 보겠음
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- true
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 2)
+	c <- person + "is sexy"
 }
